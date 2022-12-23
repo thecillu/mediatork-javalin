2c2d88e7be78b7eec14a182c73ee3d164272e78f
Filippo cilluffo <filippo.cilluffo@flowe.com>
2022-12-23 15:33:12 +0100

    Moved to InMemoryMessageBroker

--- src/main/kotlin/com/cillu/mediator/plainkotlin/application/MediatorFactory.kt
+++ src/main/kotlin/com/cillu/mediator/plainkotlin/application/MediatorFactory.kt
@@ -3,6 +3,7 @@ package com.cillu.mediator.plainkotlin.application
 import com.cillu.mediator.IMediator
 import com.cillu.mediator.MediatorFactory
 import com.cillu.mediator.messagebrokers.MessageBrokerFactory
+import com.cillu.mediator.messagebrokers.local.InMemoryMessageBroker
 import com.cillu.mediator.messagebrokers.rabbitmq.RabbitMQConfiguration
 import com.cillu.mediator.plainkotlin.domain.repositories.IMyAggregateRepository
 import com.cillu.mediator.plainkotlin.domain.services.ICounterService
@@ -17,19 +18,10 @@ class MediatorFactory {
             var serviceRegistry = ServiceRegistry()
             serviceRegistry.register(IMyAggregateRepository::class.java, MyAggregateRepository())
             serviceRegistry.register(ICounterService::class.java, CounterService())
-            var rabbitMQConfiguration = RabbitMQConfiguration(
-                host = "localhost",
-                port = 5672,
-                username = "guest",
-                password = "guest",
-                useSslProtocol = false,
-                exchangeName = "plain-kotlin-topic",
-                queueName = "plain-kotlin",
-                consumerRetryLimit = 5
-            )
             return MediatorFactory.getDefaultMediator(
                 listOf("com.cillu.mediator.plainkotlin"),
-                serviceRegistry, MessageBrokerFactory.getMessageBroker(rabbitMQConfiguration)
+                serviceRegistry,
+                InMemoryMessageBroker()
             )
         }
     }
